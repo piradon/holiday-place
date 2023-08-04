a4d0955b3508dec79520bbcc8859eba2d1b292ac
Piotr Radon <apiotre@gmail.com>
2023-08-04 21:16:41 +0200

    added animated carousel, first skecth

--- src/Pages/Carousel.js
+++ src/Pages/Carousel.js
@@ -1,58 +1,68 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import "./Carousel.css";
 
-const TOTAL_SLIDES = 3; // n-1 in Array
-
 const Carousel = () => {
-  const [current, setCurrent] = useState(0);
-  const ref = useRef(null);
-
-  const next = () => {
-    if (current >= TOTAL_SLIDES) return;
-    else setCurrent(current + 1);
-  };
-
-  const prev = () => {
-    if (current === 0) return;
-    else setCurrent(current - 1);
-  };
-
-  const desired = (e) => {
-    setCurrent(Number(e.target.id));
-  };
+  const slides = [
+    "Slide 1",
+    "Slide 2",
+    "Slide 3",
+    "Slide 4",
+    "Slide 5",
+    "Slide 6",
+    "Slide 7",
+    "Slide 8",
+    "Slide 9",
+    "Slide 10",
+    "Slide 11",
+    "Slide 12",
+    "Slide 13",
+    "Slide 14",
+    "Slide 15",
+    "Slide 16",
+    "Slide 17",
+    "Slide 18",
+    "Slide 19",
+    "Slide 20",
+    "Slide 20",
+    "Slide 20",
+    "Slide 20",
+    "Slide 20",
+    "Slide 20",
+    "Slide 20",
+    "Slide 20",
+  ]; // Add your 20 slides here
+  const [index, setIndex] = useState(0);
 
   useEffect(() => {
-    ref.current.style.transition = "all 0.2s ease-in-out";
-    ref.current.style.transform = `translateX(-${current*50}%)`;
-  }, [current]);
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => {
+        const newIndex = prevIndex + 1;
+        if (newIndex === slides.length) {
+          return 0; // Reset to the first slide once it reaches the last slide
+        } else {
+          return newIndex;
+        }
+      });
+    }, 10000); // Change slides every 2 seconds
+
+    return () => clearInterval(interval); // Clean up on component unmount
+  }, [slides.length]);
 
   return (
-    <div className="wrapperr">
-      <p>{current}</p>
-      <div className="frame">
-        <div className="box-container" ref={ref}>
-          <div className="boxx">0</div>
-          <div className="boxx">1</div>
-          <div className="boxx">2</div>
-          <div className="boxx">3</div>
-        </div>
-      </div>
-      <div className="button-container">
-        <div className="button" onClick={prev}>
-          Left
-        </div>
-        <div className="button" onClick={next}>
-          Right
-        </div>
-      </div>
-      <div className="button-2-container">
-        {[0, 1, 2, 3].map((num) => (
-          <div
-            className={`button-2 ${num === current && "active"}`}
-            onClick={desired}
-            id={num}
-            key={num}
-          />
+    <div className="carousel">
+      <div
+        className="carousel-slides"
+        style={{
+          transform: `translateX(-${index * 500}%)`,
+          //transitionTimingFunction: "ease-out",
+          transitionDuration: "3s",
+          transitionTimingFunction: "cubic-bezier(0,.1)",
+        }}
+      >
+        {slides.map((slide, i) => (
+          <p key={i} className={index === i ? "active" : ""}>
+            {slide}
+          </p>
         ))}
       </div>
     </div>
@@ -62,3 +72,39 @@ const Carousel = () => {
 export default Carousel;
 
 ////https://www.phind.com/agent?cache=clkvstygq001omm085isgqx59
+
+//// primary version
+
+// const Carousel = () => {
+//   const slides = ['Slide 1', 'Slide 2', 'Slide 3', 'Slide 4', 'Slide 5','Slide 6','Slide 7','Slide 8','Slide 9',]; // Add your slides here
+//   const [index, setIndex] = useState(0);
+//   const [stopIndex, setStopIndex] = useState(7);
+
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       setIndex((prevIndex) => {
+//         const newIndex = prevIndex + 1 === slides.length ? 0 : prevIndex + 1;
+//         if (newIndex === stopIndex) {
+//           clearInterval(interval); // Stop the interval when reaching the stopIndex
+//         }
+//         return newIndex;
+//       });
+//     }, 300); // Change slides every 2 seconds
+
+//     //return () => clearInterval(interval); // Clean up on component unmount
+//   }, [slides.length, stopIndex]);
+
+//   return (
+//     <div className="carousel">
+//       <div className="carousel-slides" style={{ transform: `translateX(-${index * 33.33}%)` }}>
+//         {slides.map((slide, i) => (
+//           <p key={i} className={index === i ? 'active' : ''}>{slide}</p>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Carousel;
+
+///
