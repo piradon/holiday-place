264105622f37ac9fe6e79ff7f96315af4ce51a2d
Piotr Radon <apiotre@gmail.com>
2023-10-02 23:06:59 +0200

    added markers random distribution inside country

--- src/Pages/WorldMap/WorldMap.js
+++ src/Pages/WorldMap/WorldMap.js
@@ -6,10 +6,8 @@ import { shorte } from "./shorte.js";
 
 function WorldMap() {
   useEffect(() => {
-    console.log(shorte);
-    console.log(window.innerWidth)
     const width = window.innerWidth;
-    const height = window.innerHeight-20;
+    const height = window.innerHeight - 20;
     const svg = d3
       .select("#map")
       .append("svg")
@@ -19,7 +17,6 @@ function WorldMap() {
       .attr("style", "max-width: 100%; height: auto;")
       .on("click", reset);
     let projection = d3.geoMercator().fitSize([width, height], shorte);
-
     const path = d3.geoPath(projection);
 
     const g = svg.append("g");
@@ -90,6 +87,53 @@ function WorldMap() {
       g.attr("transform", transform);
       g.attr("stroke-width", 1 / transform.k);
       marker.attr("transform", transform);
+    }
+
+    const gerCoord = shorte.features.filter(
+      (x) => x.properties.name === "Germany"
+    )[0];
+    var gBounds = d3.geoBounds(gerCoord);
+
+    function drawVertexSet(pointSet) {
+      g.append("circle")
+        //.data(pointSet)
+        .join("circle")
+        .attr("cx", projection(pointSet)[0])
+        .attr("cy", projection(pointSet)[1])
+        .attr("r", 1)
+        .attr(
+          "fill",
+          d3.polygonContains(gerCoord.geometry.coordinates[0], pointSet)
+            ? "blue"
+            : "red"
+        )
+        // start the circle as invisible
+        .attr("opacity", 0)
+        .transition()
+        // // how long it takes each circle to fade in
+        .duration(100)
+        // // how long to wait before transition the circle
+        .delay((d, i) => i * 100)
+        // // make the circle visible
+        .attr("opacity", 1);
+    }
+
+    console.log(gBounds);
+    const lowX = Math.floor(gBounds[0][0]);
+    const highX = Math.floor(gBounds[1][0]);
+    const lowY = Math.floor(gBounds[0][1]);
+    const highY = Math.floor(gBounds[1][1]);
+
+    console.log(lowX);
+    for (let index = 0; index < 120; index++) {
+      let constx = getRandomInRange(lowX, highX);
+      let consty = getRandomInRange(lowY, highY);
+
+      drawVertexSet([constx, consty]);
+    }
+
+    function getRandomInRange(from, to, fixed) {
+      return (Math.random() * (to - from) + from).toFixed(4);
     }
   }, []);
 
